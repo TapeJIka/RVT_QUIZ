f80de360c0a1b4508868a29c32c0928ef938f961
TapeJIka <DmitroUzumaki@yandex.ru>
2023-04-20 21:33:56 +0300

    Bez komentariem

---
+++ rvt_quiz/src/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card text-center d-flex justify-content-center align-items-center" style=" width: 280px;">
+    <h3 class="card-title">{{ title }}</h3>
+    <a class="btn btn-primary" href="views/Question_page.vue" role="button">S</a>
+  </div>
+</template>
+<script>
+export default {
+  name: "Card",
+  props: ["title"]
+};
+</script>
+
+<style scoped>
+.card{
+  padding: 15px;
+}
+.btn{
+  padding: 10px;
+}
+</style>
---
+++ rvt_quiz/src/views/Question_page.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+name: "Question_page.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
